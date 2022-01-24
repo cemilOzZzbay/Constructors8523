4d76bb5f296361d26207e50f62ab965aacd805ce
cemil özbay <cemil_ozbay@hotmail.com>
2022-01-24 23:38:47 +0300

    Add project files.

---
+++ Constructors8523/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors8523
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop1 = new Shop()
+            {
+                Id = 1,
+                Name = "Vatan"
+            };
+            Console.WriteLine("ID: " + shop1.Id + " Name: " + shop1.Name);
+            
+            Shop shop2 = new Shop(2, "Mediamarkt");
+            Console.WriteLine($"ID: {shop2.Id} Name: {shop2.Name}");
+
+            CustomerManager manager1 = new CustomerManager();
+            manager1.List();
+            CustomerManager manager2 = new CustomerManager(40);
+            manager2.List();
+
+            
+            Console.ReadLine();
+        }
+    }
+    class Shop 
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Shop() // default constructor
+        { 
+        
+        }
+        public Shop(int id,string name) // parametreli constructor - constructor overLoad
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+    class CustomerManager 
+    {
+        private int _count = 20;
+
+        public CustomerManager() 
+        {
+            //_count = 20; // İlk değer ataması Constructor'da da yapılabilir
+        }
+        public CustomerManager(int count) 
+        {
+            _count = count;
+        }
+        
+        public void List() 
+        {
+            Console.WriteLine("Listed " + _count + " items");
+        }
+    }
+}
---
+++ IDisposableKullanimi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposableKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Id = 1,
+                Name="Çağıl"
+            };
+            Console.WriteLine($"ID: {person.Id} Name: {person.Name}");
+            person.Dispose();
+            Console.ReadLine();
+        }
+    }
+    class Person:IDisposable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person()
+        {
+            Console.WriteLine("Person constructor executed.");
+        }
+        #region Dispose Operation
+        protected bool disposed = false;
+        protected virtual void Dispose(bool disposing) 
+        {
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);             //(System.IDisposable.Dispose)
+            GC.SuppressFinalize(this); //(System.GC(Garbage Collector).SuppressFinalize)
+        }
+        #endregion
+    }
+}
